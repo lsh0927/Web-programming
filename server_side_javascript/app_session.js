d8a1852d8eb53c168aef3ac16df8f9e6b1417b8e
lsh0927 <lsheon0927@naver.com>
2023-03-10 15:33:35 +0900

    session

---
+++ server_side_javascript/app_session.js
@@ -0,0 +1,29 @@
+var express= require('express');
+var session=require('express-session');
+
+var app=express();
+app.use(session({
+    secret:'1234qwefqsdcweqwe23!@',
+    resave: false,
+    saveUninitialized:true
+}));
+
+app.get('/count',function(req,res){
+    if(req.session.count){
+        req.session.count++;
+    }
+    else{
+        req.session.count=1;
+    }
+    
+    res.send('count:' +req.session.count);
+});
+
+
+app.get('/tmp',function(req,res){
+    res.send('result: '+req.session.count);
+});
+
+app.listen(3003,function(){
+    console.log('connected 3003 port!!!');
+});
