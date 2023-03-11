42e04b9c969a88049ec5085e53a5448b5965c208
lsh0927 <lsheon0927@naver.com>
2023-03-11 12:40:28 +0900

    login

--- server_side_javascript/app_session.js
+++ server_side_javascript/app_session.js
@@ -1,12 +1,13 @@
 var express= require('express');
-var session=require('express-session');
-
-var app=express();
+var session=require('express-session');//메모리에 저장
+var bodyParser=require('body-parser');
+var app=express(); //
 app.use(session({
     secret:'1234qwefqsdcweqwe23!@',
     resave: false,
     saveUninitialized:true
 }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/count',function(req,res){
     if(req.session.count){
@@ -17,6 +18,67 @@ app.get('/count',function(req,res){
     }
     
     res.send('count:' +req.session.count);
+});
+app.get('/welcome',function(req,res){
+    if(req.session.displayName)
+    {
+        res.send(`
+        <h1> Hello, ${req.session.displayName} </h1>
+        <a href="/auth/logout">logout</a>
+        `);
+    }
+    else{
+        res.send(`
+        <h1>Welcome</h1>
+        <a href="/auth/login">Login</a>
+        `);
+    }
+   
+});
+
+app.get('/auth/logout',function(req,res){
+    
+    delete req.session.displayName;
+    res.redirect('/welcome');
+    
+});
+app.post('/auth/login', function(req,res){
+    var user={
+        username:'lsh0927',
+        password:'dltmdgjs',
+        displayName:'이승헌님'
+    };
+    var uname=req.body.username;
+    var pwd=req.body.password;
+    
+    if(uname===user.username && pwd===user.password){
+        req.session.displayName=user.displayName;
+        
+        res.redirect('/welcome');
+    }
+    else{
+        res.send('who are you?<a href="/auth/login">login</a>' );
+    }
+   
+});
+
+app.get('/auth/login',function(req,res){
+    var output=`
+    <h1>Login</h1>
+    <form action="/auth/login" method="post">
+        <p>
+            <input type="text" name="username" placeholder="username">
+        </P>
+        <p>
+            <input type="password" name="password" placeholder="password">
+        </P>
+        <p>
+            <input type="submit">
+        </P>
+    </form>
+    `;
+
+    res.send(output);
 });
 
 
